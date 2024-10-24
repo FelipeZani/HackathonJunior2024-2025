0696118c1eeb85198b91e7a956f2fd0b2d51a9ca
zoroya95 <125991442+zoroya95@users.noreply.github.com>
2024-10-24 16:01:29 +0200

    Update role.py

--- classes/role.py
+++ classes/role.py
@@ -5,14 +5,11 @@ class Role:
     __type = None
     __stats = None
 
-    def __init__(self, nom, type, stats):
+    def __init__(self, nom, stats):
         self.__nom = nom
-        self.__type = type
         self.__stats = stats
 
         def getNom(nom):
             return self.__nom
-        def getType(type):
-            return self.__type
         def getStats(stats):
             return self.__stats
