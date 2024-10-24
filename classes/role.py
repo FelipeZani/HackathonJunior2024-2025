64f81f200f19ccb7f177c120ac1be37a7fc9556e
zoroya95 <125991442+zoroya95@users.noreply.github.com>
2024-10-24 15:22:31 +0200

    Add files via upload

---
+++ classes/role.py
@@ -0,0 +1,18 @@
+from hstatistics import Statistic
+
+class Role:
+    __nom = None
+    __type = None
+    __stats = None
+
+    def __init__(self, nom, type, stats):
+        self.__nom = nom
+        self.__type = type
+        self.__stats = stats
+
+        def getNom(nom):
+            return self.__nom
+        def getType(type):
+            return self.__type
+        def getStats(stats):
+            return self.__stats
