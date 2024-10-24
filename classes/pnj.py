747b9925571a505e9bc7be9f6a0133620f5c1c89
zoroya95 <125991442+zoroya95@users.noreply.github.com>
2024-10-24 16:26:11 +0200

    Update pnj.py

--- classes/pnj.py
+++ classes/pnj.py
@@ -1,1 +1,6 @@
 from alive import Alive
+
+class pnj(Alive):
+    
+    def __init__(self,name,stats,race,hostility,role,skills,inventory,equipment):
+        super.__init__(name,stats,race,hostility,role,skills,inventory,equipment)
