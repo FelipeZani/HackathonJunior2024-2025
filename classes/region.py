0de5412c5df396743fea2edbe8668213c3de7616
Zani Felipe <62755729+FelipeZani@users.noreply.github.com>
2024-10-24 16:41:03 +0200

    adding landMarks to region.py

--- classes/region.py
+++ classes/region.py
@@ -1,16 +1,26 @@
+class landMarks: 
+    CITY = "City"
+    DUNGEON = "Dungeon"
+    TOWER = "Tower"
+    KEEPS = "Keeps"
+    VILLAGES = "Villages"
+
 class region:
-    __population = None
-    __topography = None
-    __coordinates = None
-    def __init__(self,population,topography,coordinates):
+    def __init__(self, population, topography, coordinates, landMark):
         self.__population = population
         self.__topography = topography
         self.__coordinates = coordinates
+        self.__landMark = landMark  # Already storing the landmark passed to constructor
 
     def getPopulation(self):
         return self.__population
+
     def getTopography(self):
         return self.__topography
+
     def getCoordinates(self):
         return self.__coordinates
-    
+
+    # Method to return the string name for the landmark
+    def getLandMark(self):
+        return self.__landMark  # Directly return the stored landmark
