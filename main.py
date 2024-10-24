cfdba37579d8b2fe34a44cd91963d9919f78c687
Firemuu <106168942+Firemuu@users.noreply.github.com>
2024-10-24 17:25:41 +0200

    main added

---
+++ main.py
@@ -0,0 +1,27 @@
+from classes.alive import alive
+from classes.equipment import equipment
+from classes.building import building
+from classes.shop import shop
+from classes.chest import chest
+from classes.inventory import Inventory
+from classes.hstatistic import statistic
+from classes.race import race
+from classes.role import role 
+from classes.item import item
+from classes.player import player
+from classes.skills import skills
+from classes.npc import npc
+from classes.region import region
+from classes.creature import creature
+from classes.consumable import consumable
+
+
+ 
+
+def main():
+    print("Bienvenue dans la creation de votre RPG !!!")
+    newObject = race('raciste','France','gros porc',None)
+    
+    print(newObject.getName())
+    return 0
+
