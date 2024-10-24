9a343a420db88d60d4af57848b84b86469ffef76
zoroya95 <125991442+zoroya95@users.noreply.github.com>
2024-10-24 14:40:03 +0200

    Update statistic.py

---
+++ Statistic.py
@@ -0,0 +1,36 @@
+class Statistic:
+    __health = None
+    __mana = None
+    __attack= None
+    __defense = None
+    __element = None
+    __racism = None
+    __crit = None
+    __critModifier = None
+
+    def __init(self, health, mana, attack, defense, element, racism, crit, critModifier):
+        self.__health = health
+        self.__mana = mana
+        self.__attack = attack
+        self.__defense = defense
+        self.__element = element
+        self.__racism = racism
+        self.__crit = crit
+        self.__critModifier = critModifier
+
+        def getHealth(health):
+            return self.__health
+        def getMana(mana):
+            return self.__mana
+        def getAttack(attack):
+            return self.__attack
+        def getDefense(defense):
+            return self.__defense
+        def getElement(element):
+            return self.__element
+        def getRacism(racism):
+            return self.__racism
+        def getCrit(crit):
+            return self.__crit
+        def CritModifier(critModifier):
+            return self.__critModifier        
