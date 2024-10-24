93fb8b51502b887376c6e37bfaad827c4133fdbf
W4v3-cmd <charlymayeur3@gmail.com>
2024-10-24 18:43:11 +0200

    main update

--- main.py
+++ main.py
@@ -16,19 +16,23 @@ from classes.creature import creature
 from classes.consumable import consumable
 
 def createStat():
-    health = input("Choose the amount of Health from 1 to 999 999 999: ")
-    mana = input("Choose the amount of Mana from 1 to 255 :")
-    attack= input("Choose the attack from 1 to 255 : ")
-    defense = input("Choose the defense from 1 to 255 : ")
+     
+    health = int(input("Choose the amount of Health from 1 to 999 999 999: "))
+    mana = int(input("Choose the amount of Mana from 1 to 255 :"))
+    attack= int(input("Choose the attack from 1 to 255 : "))
+    defense = int(input("Choose the defense from 1 to 255 : "))
     element = 0
-    racism = input("Choose the stat of racism of the Player from 1 to JM Lepen (999 999 999) : ")
-    crit = input("Choose the probability of crit attack from 0 to 1 : ")
-    critModifier = input("Choose the multiplier of a crit attack from 1 to 1000 : ")
+    racism = int(input("Choose the stat of racism of the Player from 1 to JM Lepen (999 999 999) : "))
+    crit = float(input("Choose the probability of crit attack from 0 to 1 : "))
+    critModifier = int(input("Choose the multiplier of a crit attack from 1 to 1000 : "))
+    assert (0<health<=999_999_999) and (0<mana<=255) and (0<attack<=255) and (0<defense<=255) and (0<racism<=999_999_999 )and (0<=crit<=1) and (0<critModifier<=1000), 'Respect  the min and max amount for each stat'
     s = statistic(health,mana,attack,defense,element,racism,crit,critModifier)
+    
     return s
 
 def main():
     s = createStat()
-    Ls = [s.getHealth(),s.getMana(),s.getAttack(),s.getDefense(),s.getElement(),s.getRacism(),s.getCrit(),s.getCritModifier]
+    Ls = [s.getHealth(),s.getMana(),s.getAttack(),s.getDefense(),s.getElement(),s.getRacism(),s.getCrit(),s.getCritModifier()]
+    print(Ls)
     return 0
 
