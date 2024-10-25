906331aee25345bef2f8b389a3ba6a8430849a7f
BINGO-CRASHER <haitamhgamesh@gmail.com>
2024-10-25 04:23:02 +0200

    Update equippable.py

--- classes/equippable.py
+++ classes/equippable.py
@@ -3,5 +3,5 @@ from classes.item import item
 class equippable(item):
 
     def __init__(self, weight,type, stats):
-        super.__init__(weight,type, stats)
+        super().__init__(weight,type, stats)
 
