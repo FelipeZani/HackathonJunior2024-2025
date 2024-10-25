2c006c3510e8c4aa477957152f9c0aa83642c651
BINGO-CRASHER <haitamhgamesh@gmail.com>
2024-10-25 04:21:35 +0200

    Update creature.py

--- classes/creature.py
+++ classes/creature.py
@@ -3,4 +3,4 @@ from classes.alive import alive
 class creature(alive):
     
     def __init__(self,name,stats,race,hostility,role,skills,inventory,equipment):
-        super.__init__(name,stats,race,hostility,role,skills,inventory,equipment)
+        super().__init__(name,stats,race,hostility,role,skills,inventory,equipment)
