e005c0a4f86ac1c9451a773676cae2f55e2b9764
BINGO-CRASHER <haitamhgamesh@gmail.com>
2024-10-25 04:24:45 +0200

    Update leggings.py

--- classes/leggings.py
+++ classes/leggings.py
@@ -2,4 +2,4 @@ from classes.item import item
 
 class leggings:
     def __init__(self, name, weight, stats):
-        super.__init__(name, weight, stats)
+        super().__init__(name, weight, stats)
