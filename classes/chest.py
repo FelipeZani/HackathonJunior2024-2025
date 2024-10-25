7c22dc4ed04cd098013179c4d3caa3cafc20edde
Zani Felipe <62755729+FelipeZani@users.noreply.github.com>
2024-10-25 10:28:46 +0200

    Update chest.py

--- classes/chest.py
+++ classes/chest.py
@@ -3,9 +3,13 @@ from classes.building import building
 class chest(building):
 
     __inventory = None
-    def __init__(self,name,architecture,owner,inventory):
+    __build = None
+    def __init__(self,name,architecture,owner,inventory,build):
         super().__init__(name,architecture,owner)
         self.__inventory = inventory
+        self.__build = build
 
     def getInventory(self):
         return self.__inventory
+    def getBuild(self):
+        return self.__build
