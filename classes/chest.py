ce2adab080ea03f840b75b0731c68a82e063c1b5
BINGO-CRASHER <haitamhgamesh@gmail.com>
2024-10-25 04:20:55 +0200

    Update chest.py

--- classes/chest.py
+++ classes/chest.py
@@ -4,7 +4,7 @@ class chest(building):
 
     __inventory = None
     def __init__(self,name,architecture,owner,inventory):
-        super.__init__(name,architecture,owner)
+        super().__init__(name,architecture,owner)
         self.__inventory = inventory
 
     def getInventory(self):
