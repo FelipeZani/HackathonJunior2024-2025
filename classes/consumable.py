a27b1945c058506e8daacfe7fef3a414108bdb67
BINGO-CRASHER <haitamhgamesh@gmail.com>
2024-10-25 04:21:20 +0200

    Update consumable.py

--- classes/consumable.py
+++ classes/consumable.py
@@ -2,5 +2,5 @@ from classes.item import item
 
 class consumable(item):
     def __init__(self, weight,type, stats):
-        super.__init__(weight,type, stats)
+        super().__init__(weight,type, stats)
 
