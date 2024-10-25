e0bd803fb5f734d7cb3c11a4d06dcfb1df5db55f
W4v3-cmd <charlymayeur3@gmail.com>
2024-10-25 03:24:23 +0200

    add classes helmet, chestplate, leggings, boots

---
+++ classes/chestplate.py
@@ -0,0 +1,5 @@
+from classes.item import item
+
+class chestplate:
+    def __init__(self, name, weight, stats):
+        super.__init__(name, weight, stats)
---
+++ classes/helmet.py
@@ -0,0 +1,6 @@
+from classes.item import item
+
+class helmet:
+    def __init__(self, name, weight, stats):
+        super.__init__(name, weight, stats)
+        
