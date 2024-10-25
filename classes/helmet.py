c2888c7344ae8bfedb7049f1fa50fb4d6f5065ae
BINGO-CRASHER <haitamhgamesh@gmail.com>
2024-10-25 04:23:12 +0200

    Update helmet.py

--- classes/helmet.py
+++ classes/helmet.py
@@ -2,5 +2,5 @@ from classes.item import item
 
 class helmet:
     def __init__(self, name, weight, stats):
-        super.__init__(name, weight, stats)
+        super().__init__(name, weight, stats)
         
